52f00d35fcc8c4d2cc3b821a03fcb177204bc85e
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-11-11 15:06:49 +0000

    increased user validation when interacting with accommodations

--- src/lib/auth/jwtAuth.js
+++ src/lib/auth/jwtAuth.js
@@ -1,5 +1,6 @@
 import createHttpError from "http-errors";
 import { verifyAccessToken } from "./tools.js";
+import AccommodationsModel from "../../api/accommodation/model.js";
 
 export const JWTAuthMiddleware = async (req, res, next) => {
   if (!req.headers.authorization) {
@@ -18,26 +19,74 @@ export const JWTAuthMiddleware = async (req, res, next) => {
         role: payload.role,
       };
 
-      //   HOST-ONLY validator
-      const requestOptions = { baseUrl: req.originalUrl, method: req.method };
+      //   HOST-ONLY validator - checks if user is host when interacting with HOST-ONLY endpoints
+      const requestOptions = {
+        originalUrl: req.originalUrl,
+        method: req.method,
+      };
 
       const hostOnlyEndpoints = [
         { baseUrl: "/users/me/accommodations", method: "GET" },
         { baseUrl: "/accommodations", method: "POST" },
-        { baseUrl: "/accommodations/:id", method: "PUT" },
-        { baseUrl: "/accommodations/:id", method: "DELETE" },
+        { baseUrl: "/accommodations", method: "PUT" },
+        { baseUrl: "/accommodations", method: "DELETE" },
       ];
 
       const endpointIndex = hostOnlyEndpoints.findIndex(
         (endpoint) =>
-          endpoint.baseUrl === requestOptions.baseUrl &&
+          (endpoint.baseUrl === requestOptions.originalUrl ||
+            endpoint.baseUrl === req.baseUrl) &&
           endpoint.method === requestOptions.method
       );
       const isThisAHostOnlyReq = endpointIndex === -1 ? false : true;
 
       if (isThisAHostOnlyReq) {
         if (req.user.role === "Host") {
-          next();
+          if (requestOptions.method === "GET") {
+            next();
+          } else {
+            if (requestOptions === "POST") {
+              // Checks if posting user is specified user within the new accommodation data in request body
+
+              if (req.body.host === req.user._id) {
+                next();
+              } else {
+                next(
+                  createHttpError(
+                    403,
+                    "You may not post a new accommodation without you as the host, if you wish to post under this account please log into the account and post there."
+                  )
+                );
+              }
+            } else {
+              //   Checks if user is host of accommodation in case of PUT or DELETE of accommodation
+
+              const accommodation = await AccommodationsModel.findById(
+                req.params.id
+              );
+
+              if (accommodation) {
+                if (accommodation.host.toString() === req.user._id) {
+                    
+                  next();
+                } else {
+                  next(
+                    createHttpError(
+                      403,
+                      "You may not edit or delete another users post."
+                    )
+                  );
+                }
+              } else {
+                next(
+                  createHttpError(
+                    404,
+                    `Accommodation with id ${req.params.id} not found.`
+                  )
+                );
+              }
+            }
+          }
         } else {
           next(
             createHttpError(403, "Must be a host to access these endpoints!")
