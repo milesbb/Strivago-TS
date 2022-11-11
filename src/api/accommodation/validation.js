406c4f5092e6a550ca9b0bf4b011d97bc360b77a
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-11-11 12:31:07 +0000

    added user registration endpoint

---
+++ src/api/accommodation/validation.js
@@ -0,0 +1,53 @@
+import { checkSchema, validationResult } from "express-validator";
+import createHttpError from "http-errors";
+
+const accommodationSchema = {
+  name: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Name is a mandatory field and must be a string/text",
+    },
+  },
+  host: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Host is a mandatory field and must be a string/text",
+    },
+  },
+  description: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Description is a mandatory field and must be a string/text",
+    },
+  },
+  maxGuests: {
+    in: ["body"],
+    isInt: {
+      errorMessage:
+        "Max Guests is a mandatory field and must be an integer",
+    },
+  },
+  city: {
+    in: ["body"],
+    isString: {
+      errorMessage: "City is a mandatory field and must be a string/text",
+    },
+  },
+};
+
+export const checkAccommodationSchema = checkSchema(accommodationSchema);
+
+export const checkValidationResult = (req, res, next) => {
+  const errorsList = validationResult(req);
+  if (!errorsList.isEmpty()) {
+    next(
+      createHttpError(
+        400,
+        "Validation error in request! Error(s) are displayed below",
+        { errorsList: errorsList.array() }
+      )
+    );
+  } else {
+    next();
+  }
+};
---
+++ src/api/user/validation.js
@@ -0,0 +1,44 @@
+import { checkSchema, validationResult } from "express-validator";
+import createHttpError from "http-errors";
+
+const usersSchema = {
+  email: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Email is a mandatory field and must be a string/text",
+    },
+  },
+  password: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Password is a mandatory field and must be a string/text",
+    },
+  },
+  role: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Role is a mandatory field and must be a string/text",
+    },
+    isIn: {
+      options: ["Host,Guest"],
+      errorMessage: "Role must be either 'Host' or 'Guest'",
+    },
+  },
+};
+
+export const checkUsersSchema = checkSchema(usersSchema);
+
+export const checkValidationResult = (req, res, next) => {
+  const errorsList = validationResult(req);
+  if (!errorsList.isEmpty()) {
+    next(
+      createHttpError(
+        400,
+        "Validation error in request! Error(s) are displayed below",
+        { errorsList: errorsList.array() }
+      )
+    );
+  } else {
+    next();
+  }
+};
